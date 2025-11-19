58ad9371e822d9ba3c40a84fbad99ee549275210
addempirellc <addempirebrand@gmail.com>
2025-11-19 15:47:22 -0500

    membership-codes.js

---
+++ membership-codes.js
@@ -0,0 +1,17 @@
+// Lista central de códigos de artista / membresía.
+// Aquí es donde vas pegando los códigos nuevos.
+// IMPORTANTE: siempre en mayúsculas para que la validación sea consistente.
+
+window.validCodes = [
+  // Key Tier
+  "ADD-KEY-001",
+  "ADD-KEY-002",
+  "ADD-KEY-003",
+
+  // Rising Tier
+  "ADD-RISE-001",
+  "ADD-RISE-002",
+
+  // Crown Tier
+  "ADD-CROWN-001"
+];
